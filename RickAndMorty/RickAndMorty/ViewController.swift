5ae5cc0dfc5d123f6e460acf7b761c30f5652a3e
Igor Terletskyi <ihorek12@gmail.com>
2025-04-30 13:54:34 +0300

    First commit with initial project
    - remove storyboard from the project

---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Igor Terletskyi on 30.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
